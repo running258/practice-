f4bab895f1c193c37a2ae4e47919b7edc623b12f
zhangleyuan <zhangleyuan@viewhigh.com>
2018-12-19 10:33:01 +0800

    1

--- common/readYaml.py
+++ common/readYaml.py
@@ -1,3 +1,11 @@
 import os
 from yaml import load
 
+class yamlFile(object):
+
+    def __init__(self,path='ele'):
+        self.fileRoot = os.path.abspath('.')+'\\'+path
+
+    def readYamlFile(self,fileName):
+        with open(self.fileRoot+fileName,'r') as _yamlFile:
+            return load(_yamlFile)
--- test_supplyLogin.py
+++ test_supplyLogin.py
@@ -1,27 +1,25 @@
 from selenium import webdriver
 import pytest
 from conf.envInit import envInit
-# from ele.supply.login import loginEle
+from common.readYaml import yamlFile
 
+loginEle = yamlFile().readYamlFile('\\supply\\ele_loginPage.yaml')
 
 @pytest.fixture(scope='function')
 def browserLogin():
     browser = envInit().openBrowser()
-    userName = browser.find_element_by_xpath(
-        "//input[@id='viewhigh-login-username-input']")
-    passWord = browser.find_element_by_xpath(
-        "//input[@id='viewhigh-login-pwd-input']")
-    loginButton = browser.find_element_by_xpath(
-        "//button[@id='viewhigh-login-login-btn']")
+    userName = browser.find_element_by_xpath(loginEle['supply_loginPage']['userName'])
+    passWord = browser.find_element_by_xpath(loginEle['supply_loginPage']['password'])
+    loginButton = browser.find_element_by_xpath(loginEle['supply_loginPage']['loginButton'])
     userName.send_keys("zhangleyuan")
     passWord.send_keys("zhangleyuan")
     loginButton.click()
     yield browser
     browser.close()
 
-
 # TODO   *
 def test_loginSuccess(browserLogin):
     browserLogin.implicitly_wait(5)
     assert browserLogin.find_element_by_xpath(
         "//*[@class='icon icon-caidan']").is_enabled()
+
